f12dd088424078bca6ff1ff465e64478bcfae2dc
김진현 <kimjhyun0627@gmail.com>
2025-05-21 20:41:23 +0900

    initial commit

---
+++ src/main/java/com/example/ktcspringschedulercrud/KtcSpringSchedulerCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.ktcspringschedulercrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtcSpringSchedulerCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtcSpringSchedulerCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ktcspringschedulercrud/KtcSpringSchedulerCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ktcspringschedulercrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtcSpringSchedulerCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
